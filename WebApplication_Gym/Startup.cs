03e78f00d1f627aeb69ab5db90df72e355763bcb
Shane <43347614+andersonshane@users.noreply.github.com>
2019-04-13 14:50:31 +0100

    Add project files.

---
+++ WebApplication_Gym/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_Gym.Startup))]
+namespace WebApplication_Gym
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
